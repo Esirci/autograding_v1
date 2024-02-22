27a428ae2a5d83605033fd321cb7077381dc3814
fne <fatmanuresirci@gmail.com>
2024-02-22 19:22:50 +0300

    Update eval.py

--- eval.py
+++ eval.py
@@ -3,19 +3,36 @@ import subprocess
 def run_job(cmd):
     ret = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     return ret.stdout.decode(('UTF-8'))
-    
-if __name__ == '__main__':
-    # Compile
-    #run_job("gcc main.c")
-    run_job("gcc -Wall -o hw2 hw2.c")
 
-    # Run
-    #msg = run_job("./a.out").rstrip()
-    msg = run_job("./hw2").rstrip()
-    if msg == "Hello World!":
-        print("Ok! expected output is corerect.")
-    else:
-        print("Error. The correct answer is `Hello World!`, but your output is:")
-        print("```")
-        print(msg)
-        print("```")
+
+
+def run_tests():
+    # main.c dosyasını derleme
+    compile_process = subprocess.run(["gcc", "hw2.c", "-o", "hw2"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    
+    if compile_process.returncode != 0:
+        print("Hata: Kod derlenemedi.")
+        print("Hata çıktısı:")
+        print(compile_process.stderr)
+        return
+    
+    # Derlenen programı çalıştırma
+    # Kullanıcıdan girdileri alarak çalıştırma
+    user_input = input("Programa girmek istediğiniz veriyi girin: ")
+    execute_process = subprocess.run(["./hw2", user_input], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    
+    if execute_process.returncode != 0:
+        print("Hata: Program çalıştırılamadı.")
+        print("Hata çıktısı:")
+        print(execute_process.stderr)
+        return
+    
+    # Program çıktısını yazdırma
+    print("Program çıktısı:")
+    print(execute_process.stdout)
+
+
+# Test fonksiyonunu çağırma
+run_tests()
+
+
